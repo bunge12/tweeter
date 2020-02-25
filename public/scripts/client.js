e92867597d6f73a7b7a50d2c77f77d5e1bdaad8a
Artur Iatsko <arturyatsko@gmail.com>
2020-02-25 22:51:18 +0000

    Code cleanup & renderTweets finished

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,8 +27,31 @@ const data = [
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
-]
+];
 
 const createTweetElement = (tweet) => {
   const $markup = `
@@ -51,15 +74,14 @@ const createTweetElement = (tweet) => {
     </article>
   `;
   return $markup;
-}
+};
 
 const renderTweets = (tweets) => {
-
   for (const tweet of tweets) {
-
     $('#tweets').append(createTweetElement(tweet));
   }
+};
 
-}
-
-renderTweets(data);
+$(document).ready(function () {
+  renderTweets(data);
+});
