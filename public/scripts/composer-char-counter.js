bdd6c0f428d954b904380303b0d2b394f8e625bf
Artur Iatsko <arturyatsko@gmail.com>
2020-02-25 20:09:41 +0000

    Counter cleaned up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,6 @@
+/* Function to count of entered characters, display the CountQueuingStrategy, and update the counter color to red when chracter count exceeds 140 chars */
+
 $(document).ready(function () {
-  /*         const count = document.getElementById('new_tweet');
-          document.addEventListener('keyup', updateCounter);
-          function updateCounter(e) {
-            // let curr = document.getElementById("char_count").innerHTML;
-            // document.getElementById("char_count").innerHTML = curr - 1;
-            document.getElementById("char_count").innerHTML = 140 - count.value.length;
-          } */
   $("#new_tweet").on('keyup', function () {
     $("#char_count").html(140 - this.value.length);
     if (this.value.length > 140) {
@@ -13,5 +8,5 @@ $(document).ready(function () {
     } else {
       $("#char_count").css("color", "#545149");
     }
-  })
+  });
 });
