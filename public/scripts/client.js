88a5fe0c1d79398a1c2f9fd2b7fc11ab2ca3bd33
Artur Iatsko <arturyatsko@gmail.com>
2020-02-25 22:19:25 +0000

    Single tweet merging function added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweet) => {
+  const $markup = `
+    <article class="single_tweet">
+    <header>
+      <img src="${tweet.user.avatars}">
+      <h2>${tweet.user.name}</h2>
+      <span class='username right'>${tweet.user.handle}</span>
+    </header>
+    <p>${tweet.content.text}</p>
+    <hr />
+    <footer>
+      <span>${tweet.created_at}</span>
+      <span class='right'>
+        <img src="/images/flag.png" alt="Flag Item icon">
+        <img src="/images/loop.png" alt="Repost Item icon">
+        <img src="/images/heart.png" alt="Like Item icon">
+      </span>
+    </footer>
+  </article>
+`;
+  return $markup;
+}
